df202380a8eaabae72ebd5a61ae8f12a7808b121
Angel Espinosa <zS24013272@estudiantes.uv.mx>
2026-04-17 15:57:22 -0600

    test update

---
+++ src/test/java/daotest/AdministratorDAOImplementationTest.java
@@ -0,0 +1,22 @@
+package daotest;
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import spp.data.repository.implementation.AdministratorDAOImplementation;
+import spp.domain.dto.AdministratorDTO;
+
+
+public class AdministratorDAOImplementationTest {
+    @Test
+    void shouldSaveUserSuccessfully() {
+        AdministratorDAOImplementation adminDAO = new AdministratorDAOImplementation();
+        AdministratorDTO admin = new AdministratorDTO();
+        admin.setUserAccount("AAAA");
+        admin.setUserId(1);
+        
+        boolean result = adminDAO.save(admin);
+        
+        assertTrue(result);
+    }
+}
