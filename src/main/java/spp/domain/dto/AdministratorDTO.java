656baec1ca51b0ff646410809a6464c180191f5e
Angel Espinosa <zS24013272@estudiantes.uv.mx>
2026-04-24 13:23:38 -0600

    add(AdministratorDAOImplementationTest): test validation

--- src/main/java/spp/domain/dto/AdministratorDTO.java
+++ src/main/java/spp/domain/dto/AdministratorDTO.java
@@ -1,5 +1,7 @@
 package spp.domain.dto;
 
+import java.util.Objects;
+
 public class AdministratorDTO {
     private int id;
     private String userAccount;
@@ -28,8 +30,30 @@ public class AdministratorDTO {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-    
-    
 
-    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.userAccount);
+        hash = 47 * hash + this.userId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AdministratorDTO other = (AdministratorDTO) obj;
+        if (this.userId != other.userId) {
+            return false;
+        }
+        return Objects.equals(this.userAccount, other.userAccount);
+    }
 }
