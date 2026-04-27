c88dcabcfafd25bd73a4d7cc7890faecb4c7ed71
Angel Espinosa <zS24013272@estudiantes.uv.mx>
2026-04-27 09:27:27 -0600

    add(test) userImplementationTest

--- src/test/java/daotest/UserDAOImplementationTest.java
+++ src/test/java/daotest/UserDAOImplementationTest.java
@@ -1,77 +1,145 @@
-//package daotest;
+package daotest;
 
 
-//import org.junit.jupiter.api.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//import spp.data.repository.implementation.UserDAOImplementation;
-//import spp.domain.dto.UserDTO;
-//import spp.data.exception.PersistenceException;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import spp.data.connection.ConnectionPool;
+import spp.data.repository.implementation.UserDAOImplementation;
+import spp.data.exception.PersistenceException;
+import spp.data.exception.ConfigurationException;
+import spp.domain.dto.UserDTO;
+import spp.domain.enums.StatusUser;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 
-//public class UserDAOImplementationTest {
-//   @Test
-//    void shouldSaveUserSuccessfully() {
-//        UserDAOImplementation userDAO = new  UserDAOImplementation();
-       
-//        UserDTO user = new UserDTO();
-//        user.setAccount("zS202021");
-//        user.setPassword("12345");
-//        user.setStatus("Activo");
-        
-//        boolean result = userDAO.save(user);
-//        assertTrue(result);
-        
-//        UserDTO userSaved = userDAO.getByAccount("zS24202021");
-//        assertNotNull(userSaved);
-//    }
+public class UserDAOImplementationTest {
+    private static final Logger LOGGER = Logger.getLogger(
+            UserDAOImplementationTest.class.getName()
+        );
     
-//    @Test
-//    void saveUserThrowsExceptionWhenDataInvalid() {
-//        UserDAOImplementation userDAO = new  UserDAOImplementation();
+    /**
+     * CONFIG USER DATA:
+     * The user data must remain the same for each test
+     */
+    private static final String TEST_ACCOUNT = "zS25023072";
+    private static final String TEST_PASSWORD = "JLO_3315$";
+    private static final StatusUser TEST_STATUS = StatusUser.ACTIVO;
+    
+    private UserDAOImplementation userDAO;
+    
+    /**
+     * You can call createAuxiliaryUser to use an extra auxiliary user
+     * if you need it in your tests (for example, to check if it exists).
+     * Important: the SET parameters correspond to CONFIG USER DATA,
+     *            THEY SHOULD NOT BE MODIFIED
+     */
+    private void createAuxiliaryUser() throws PersistenceException{
+        UserDTO auxiliaryUser = new UserDTO();
+        auxiliaryUser.setAccount(TEST_ACCOUNT);
+        auxiliaryUser.setPassword(TEST_PASSWORD);
+        auxiliaryUser.setStatus(TEST_STATUS);
         
-//        UserDTO user = new UserDTO();
-//        user.setAccount(null);
-//        user.setPassword("12345");
-//        user.setStatus("Activo");
+        userDAO.save(auxiliaryUser);
+    }
+
+    
+    private void cleanData(String account){
+        try (Connection connection = ConnectionPool
+                .getInstanceConectionPool().getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                       "DELETE FROM usuario WHERE cuenta = ?")) {
+            preparedStatement.setString(1,account);
+            preparedStatement.executeUpdate();
+        } catch (ConfigurationException e){
+            LOGGER.log(Level.SEVERE, "property configuration error when clearing test data", e);
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Error connecting to the database", e);
+        }   
+    } 
+    
+    @BeforeEach
+    void setUp() {
+        userDAO = new UserDAOImplementation();
+    }
+    
+    @AfterEach
+    void takeOutTrash() {
         
-//        PersistenceException ex = assertThrows(
-//            PersistenceException.class,
-//            () -> userDAO.save(user)
-//        );
-//    }
-   
-//   @Test
-//   void shouldReturnUserByAccount() {
-//        UserDAOImplementation userDAO = new  UserDAOImplementation();
+        //The default account must be changed if TEST_ACCOUNT changes.
+        cleanData(TEST_ACCOUNT); 
+    }
+    
+    @Test
+    void Should_RegisterUser_When_EnteringUserData() throws PersistenceException{
+        UserDTO testUser = new UserDTO();
+        testUser.setAccount(TEST_ACCOUNT);
+        testUser.setPassword(TEST_PASSWORD);
+        testUser.setStatus(TEST_STATUS);
         
-//        UserDTO user = new UserDTO();
-//        user.setAccount("zS24013272");
-//        user.setPassword("1234");
-//        user.setStatus("Activo");
+        boolean resultTest = userDAO.save(testUser);
         
-//        userDAO.save(user);
-//        UserDTO retrievedUser = userDAO.getByAccount("zS24013272");
+        assertTrue(resultTest);
+    }
+    
+    @Test
+    void Should_ThrowPersistenceException_When_TryRegisterUserThatAlreadyExists() {
+        createAuxiliaryUser();
+
+        UserDTO testUser = new UserDTO();
+        testUser.setAccount(TEST_ACCOUNT);
+        testUser.setPassword(TEST_PASSWORD);
+        testUser.setStatus(TEST_STATUS);
         
-//        assertNotNull(retrievedUser);
-//        assertEquals("zS24013272", retrievedUser.getAccount());     
-//   }
-   
-//   @Test
-//   void shouldUpdateUserStatus() {
-//       UserDAOImplementation userDAO = new  UserDAOImplementation();
+        assertThrows(PersistenceException.class, () -> userDAO.save(testUser));       
+    }
+    
+    @Test
+    void Should_ReturnUserDTO_When_AccountExists() throws PersistenceException {
+        createAuxiliaryUser();
         
-//       UserDTO user = new UserDTO();
-//       user.setAccount("1234567891");
-//       user.setPassword("12345");
-//       user.setStatus("Activo");
+        UserDTO testExpectedUser = new UserDTO();
+        testExpectedUser.setAccount(TEST_ACCOUNT);
+        testExpectedUser.setPassword(TEST_PASSWORD);
+        testExpectedUser.setStatus(TEST_STATUS);
         
-//       userDAO.save(user);
-//       UserDTO retrievedUser = userDAO.getByAccount("1234567891");
-       
-//       boolean update = userDAO.updateStatus(retrievedUser.getId(), "Inactivo");
-//       assertTrue(update);
-       
-//       UserDTO retrievedUserUpdate = userDAO.getByAccount("1234567891");
-//       assertEquals("Inactivo", retrievedUserUpdate.getStatus());
-//   }
-//}
+        //obtains the account of the user registered in createAuxiliaryUser()
+        UserDTO userObtained = userDAO.getByAccount(TEST_ACCOUNT);
+        
+        assertEquals(testExpectedUser, userObtained);
+    }
+    
+    @Test
+    void Should_ReturnNull_When_AccountDoesNotExist() throws PersistenceException {
+        
+        //user account that does not exist
+        UserDTO testUser = userDAO.getByAccount("zS21458893");
+        
+        assertNull(testUser);
+    }
+    
+    @Test
+    void Should_UpdateUserStatus_When_UserStatusChangesUsingTheirAccount() throws PersistenceException {
+        createAuxiliaryUser();
+        
+        UserDTO testUser = userDAO.getByAccount(TEST_ACCOUNT);
+        boolean resultTest = userDAO.updateStatus(testUser.getId(), StatusUser.INACTIVO.getDatabaseValue());
+        
+        assertTrue(resultTest);
+        
+    }
+    
+    @Test 
+    void Should_ReturnFalse_When_TryUpdateUserStatusAndUserDoesNotExist() throws PersistenceException{
+        int nonExistentId = 999;
+        
+        boolean resultTest = userDAO.updateStatus(nonExistentId, StatusUser.INACTIVO.getDatabaseValue());
+        
+        assertFalse(resultTest);
+    }
+}
